a7217fd22c94d2f08d77ea0f64882fde0405b8d9
308627993 <308627993@qq.com>
2018-09-25 01:16:08 +0800

    view

--- voa/views.py
+++ voa/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 
 
 from .tools import mykindle
-import traceback
+import sys
 # Create your views here.
 
 def index(request):
     try:
         result = mykindle.main()
-    except Exception, e:
-        result = 'fail ,str(Exception):\t%s\n str(e):\t\t%s \nrepr(e):\t%s\ne.message:\t%s\ntraceback.print_exc():%s\ntraceback.format_exc():\n%s'%(str(Exception),str(e),repr(e),e.message,traceback.print_exc(),traceback.format_exc())
+    except:
+        result = sys.exc_info()
     return render(request, 'voa/index.html', {
         'result': result,
     })
