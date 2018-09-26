2c5ab9deb4183a9a2956322a93c2bfe460301262
308627993 <308627993@qq.com>
2018-09-27 00:05:52 +0800

    ss

--- weixin/tools/autoreply.py
+++ weixin/tools/autoreply.py
@@ -10,6 +10,7 @@ def delete_or_show_file(path,filetypes,action):
     for filetype in filetypes:
         files += glob.glob(os.path.join(path,filetype))
     if action == 'show':
+        print(files)
         return str(files)
     elif action == 'delete':
         for file in files:
