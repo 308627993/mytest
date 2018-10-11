72023cbc05ab20848bc94b7309f94ce3b4b92432
308627993 <308627993@qq.com>
2018-10-11 23:25:09 +0800

    aaa

--- weixin/tools/autoreply.py
+++ weixin/tools/autoreply.py
@@ -55,7 +55,7 @@ def autoreply(request):
                 message = 'make mobi file and send mail start...'
                 #from voa.tools import mykindle
                 #mykindle.main()
-                subprocess.Popen(['python ../../../voa/tools/mykindle.py &'])
+                subprocess.Popen(['python','../../../voa/tools/mykindle.py'],shell=True)
             elif content == 'mails':
                 from weixin.models import Email
                 message = 'mails total %s EA'%(len(Email.objects.all()))
