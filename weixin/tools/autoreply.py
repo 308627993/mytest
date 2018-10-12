ec007af1442d1f0e27821d151492a3aaa2be6211
308627993 <308627993@qq.com>
2018-10-12 23:05:28 +0800

    aa

--- weixin/tools/autoreply.py
+++ weixin/tools/autoreply.py
@@ -59,9 +59,9 @@ def autoreply(request):
                 #from voa.tools import mykindle
                 #mykindle.main()
             elif content == 'log':
-                message = '%s--%s'%(path, delete_or_show_file(path='%s/weixin/management/commands/voa'%django_root_path,filetypes=['*.txt','*.py'],action='show'))
-                #with open('%s/weixin/management/commands/log.txt'%django_root_path,'r') as f:
-                #    message = f.read()
+                #message = '%s--%s'%(path, delete_or_show_file(path='%s/weixin/management/commands/voa'%django_root_path,filetypes=['*.txt','*.py'],action='show'))
+                with open('%s/weixin/management/commands/voa/log.txt'%django_root_path,'r') as f:
+                    message = f.read()
 
             elif content == 'mails':
                 from weixin.models import Email
