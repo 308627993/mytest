e0a6e6bee0b1327bd68c8648a21751742fcbbbe3
308627993 <308627993@qq.com>
2018-10-13 20:51:42 +0800

    a

--- weixin/management/commands/mykindle.py
+++ weixin/management/commands/mykindle.py
@@ -8,39 +8,32 @@ import threading
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        '''
-        path = os.path.dirname(os.path.abspath(__file__))
-        def doit(order):
-            try:
-                job = subprocess.Popen(order,shell = True)
-                job.wait()
-            except:
-                pass
-        get_data_new.main() # download the voa news
-        html_files =[i for i in os.listdir('%s/voa/result'%(path)) if re.findall(r'html',i)]
-        log = ''
-        if html_files:
-            try:
-                order_makemobi = '%s/voa/kindlegen %s/voa/result/*.opf'%(path,path)
-                doit(order_makemobi) # make mobi format ebook
-                time.sleep(15) # 休眠15秒
-                sendmail.send_mail() # send mail
-                log = 'send mail success!'
-                print(log)
-            except:
-                log = 'send mail fail!'
-        else:
-            log = 'there is no html files'
-        with open('%s/voa/log.txt'%path,'w') as f:
-            log += '---%s'%datetime.date.today()
-            print(log)
-            f.write(log)
-        return log
-        '''
-        def job():
+        def everyday_job():
             path = os.path.dirname(os.path.abspath(__file__))
+            def doit(order):
+                try:
+                    job = subprocess.Popen(order,shell = True)
+                    job.wait()
+                except:
+                    pass
+            get_data_new.main() # download the voa news
+            html_files =[i for i in os.listdir('%s/voa/result'%(path)) if re.findall(r'html',i)]
+            log = ''
+            if html_files:
+                try:
+                    order_makemobi = '%s/voa/kindlegen %s/voa/result/*.opf'%(path,path)
+                    doit(order_makemobi) # make mobi format ebook
+                    time.sleep(15) # 休眠15秒
+                    sendmail.send_mail() # send mail
+                    log = 'send mail success!'
+                except:
+                    log = 'send mail fail!'
+            else:
+                log = 'there is no html files,can not make mobi file'
             with open('%s/voa/log.txt'%path,'w') as f:
-                f.write('---%s/n'%datetime.datetime.now())
-                timer = threading.Timer(3,job)
-                timer.start()
-        job()
+                log += '---%s'%datetime.datetime.now()
+                print(log)
+                f.write(log)
+            timer = threading.Timer(24*3600,everyday_job) #每天执行一次
+            timer.start()
+        everyday_job()
