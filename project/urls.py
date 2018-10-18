24e3b44d870c178088e8b1ccd49073894493d5cd
308627993 <308627993@qq.com>
2018-10-18 12:56:31 +0800

    aa

--- project/urls.py
+++ project/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    #url(r'^$', index),
+    url(r'^$', index),
     url(r'^health$', health),
     #url(r'^admin/', include(admin.site.urls)),
     url(r'^weixin/', include('weixin.urls')),
