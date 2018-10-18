9cd24a75d2288b5895fcb36bfc6c2a8c1ea29e7b
308627993 <308627993@qq.com>
2018-10-18 11:26:05 +0800

    aa

--- project/urls.py
+++ project/urls.py
@@ -2,16 +2,16 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from welcome.views import index, health
+#from welcome.views import index, health
 import weixin
 urlpatterns = [
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', index),
-    url(r'^health$', health),
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^$', index),
+    #url(r'^health$', health),
+    #url(r'^admin/', include(admin.site.urls)),
     url(r'^weixin/', include('weixin.urls')),
 ]
 
